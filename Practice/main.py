c4ac6efb4e22a6c7290826d37b03f254dd72acf9
sneha-pasam <snehapasam63@gmail.com>
2024-05-04 14:44:53 +0530

    Added Changes

---
+++ Practice/main.py
@@ -0,0 +1,21 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+n = int(input("Enter a number: "))
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+mod = a%b
+did = a//b
+exp1 = a**n
+exp2 = b**n
+if a >= b and n:
+    print(sub)
+    print(div)
+    print(did)
+    print(mod)
+    print(exp1)
+    print(exp2)
+else:
+    print(add)
+    print(mul)
